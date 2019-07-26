78a4d101c6dda052427c7dd09114022e1f9e8a72
Edward Wang <edward@jrvs.ca>
2019-07-26 13:07:18 -0400

    update docker install sh

--- GCP/docker_install.sh
+++ GCP/docker_install.sh
@@ -1,9 +1,40 @@
 #!/bin/bash
 
-yum -y install docker
+#instasll docker
+sudo yum remove -y docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum -y install docker-ce docker-ce-cli containerd.io
+
+#auto start
+sudo systemctl enable docker
+#start service
+sudo systemctl start docker
+sudo docker -v
+
+#allow centos user to run docker without sudo
 groupadd docker
 usermod -aG docker centos
-#must reboot
-reboot
-#systemctl start docker
+#must logout session
+exit
+
+#auto start docker
+#systemctl enable docker
+
+#start docker
+systemctl start docker
+#systemctl start|status|restart docker
+
 
